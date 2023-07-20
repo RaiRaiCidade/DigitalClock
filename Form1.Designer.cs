5983741dcd34316a7f0ec736bae36bb7a64e7291
RaiRai <129341896+RaiRaiCidade@users.noreply.github.com>
2023-07-19 21:12:49 -0300

    Enviando arquivos

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace DigitalClock {
+    partial class DigitalClock {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent( ) {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.clockLabel = new System.Windows.Forms.Label();
+            this.clockTimer = new System.Windows.Forms.Timer(this.components);
+            this.redButton = new System.Windows.Forms.Button();
+            this.orangeButton = new System.Windows.Forms.Button();
+            this.blueButton = new System.Windows.Forms.Button();
+            this.greenButton = new System.Windows.Forms.Button();
+            this.yellowButton = new System.Windows.Forms.Button();
+            this.styleButton1 = new System.Windows.Forms.Button();
+            this.styleButton3 = new System.Windows.Forms.Button();
+            this.styleButton2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(431, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // clockLabel
+            // 
+            this.clockLabel.Font = new System.Drawing.Font("Segoe UI", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clockLabel.ForeColor = System.Drawing.Color.Red;
+            this.clockLabel.Location = new System.Drawing.Point(1, 3);
+            this.clockLabel.Name = "clockLabel";
+            this.clockLabel.Size = new System.Drawing.Size(855, 379);
+            this.clockLabel.TabIndex = 1;
+            this.clockLabel.Text = "Loading...";
+            this.clockLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // clockTimer
+            // 
+            this.clockTimer.Interval = 1000;
+            this.clockTimer.Tick += new System.EventHandler(this.clockTimer_Tick);
+            // 
+            // redButton
+            // 
+            this.redButton.BackColor = System.Drawing.Color.Red;
+            this.redButton.Location = new System.Drawing.Point(12, 412);
+            this.redButton.Name = "redButton";
+            this.redButton.Size = new System.Drawing.Size(30, 26);
+            this.redButton.TabIndex = 2;
+            this.redButton.UseVisualStyleBackColor = false;
+            this.redButton.Click += new System.EventHandler(this.redButton_Click);
+            // 
+            // orangeButton
+            // 
+            this.orangeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.orangeButton.Location = new System.Drawing.Point(48, 412);
+            this.orangeButton.Name = "orangeButton";
+            this.orangeButton.Size = new System.Drawing.Size(30, 26);
+            this.orangeButton.TabIndex = 3;
+            this.orangeButton.UseVisualStyleBackColor = false;
+            this.orangeButton.Click += new System.EventHandler(this.orangeButton_Click);
+            // 
+            // blueButton
+            // 
+            this.blueButton.BackColor = System.Drawing.Color.DodgerBlue;
+            this.blueButton.Location = new System.Drawing.Point(156, 412);
+            this.blueButton.Name = "blueButton";
+            this.blueButton.Size = new System.Drawing.Size(30, 26);
+            this.blueButton.TabIndex = 6;
+            this.blueButton.UseVisualStyleBackColor = false;
+            this.blueButton.Click += new System.EventHandler(this.blueButton_Click);
+            // 
+            // greenButton
+            // 
+            this.greenButton.BackColor = System.Drawing.Color.Green;
+            this.greenButton.Location = new System.Drawing.Point(120, 412);
+            this.greenButton.Name = "greenButton";
+            this.greenButton.Size = new System.Drawing.Size(30, 26);
+            this.greenButton.TabIndex = 7;
+            this.greenButton.UseVisualStyleBackColor = false;
+            this.greenButton.Click += new System.EventHandler(this.greenButton_Click);
+            // 
+            // yellowButton
+            // 
+            this.yellowButton.BackColor = System.Drawing.Color.Yellow;
+            this.yellowButton.Location = new System.Drawing.Point(84, 412);
+            this.yellowButton.Name = "yellowButton";
+            this.yellowButton.Size = new System.Drawing.Size(30, 26);
+            this.yellowButton.TabIndex = 8;
+            this.yellowButton.UseVisualStyleBackColor = false;
+            this.yellowButton.Click += new System.EventHandler(this.yellowButton_Click);
+            // 
+            // styleButton1
+            // 
+            this.styleButton1.BackColor = System.Drawing.Color.White;
+            this.styleButton1.Font = new System.Drawing.Font("Impact", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.styleButton1.Location = new System.Drawing.Point(192, 414);
+            this.styleButton1.Name = "styleButton1";
+            this.styleButton1.Size = new System.Drawing.Size(104, 23);
+            this.styleButton1.TabIndex = 9;
+            this.styleButton1.Text = "Impact";
+            this.styleButton1.UseVisualStyleBackColor = false;
+            this.styleButton1.Click += new System.EventHandler(this.styleButton1_Click);
+            // 
+            // styleButton3
+            // 
+            this.styleButton3.BackColor = System.Drawing.Color.White;
+            this.styleButton3.Font = new System.Drawing.Font("Philosopher", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.styleButton3.Location = new System.Drawing.Point(412, 415);
+            this.styleButton3.Name = "styleButton3";
+            this.styleButton3.Size = new System.Drawing.Size(104, 23);
+            this.styleButton3.TabIndex = 10;
+            this.styleButton3.Text = "Philosopher";
+            this.styleButton3.UseVisualStyleBackColor = false;
+            this.styleButton3.Click += new System.EventHandler(this.styleButton3_Click);
+            // 
+            // styleButton2
+            // 
+            this.styleButton2.BackColor = System.Drawing.Color.White;
+            this.styleButton2.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.styleButton2.Location = new System.Drawing.Point(302, 415);
+            this.styleButton2.Name = "styleButton2";
+            this.styleButton2.Size = new System.Drawing.Size(104, 23);
+            this.styleButton2.TabIndex = 11;
+            this.styleButton2.Text = "Consolas";
+            this.styleButton2.UseVisualStyleBackColor = false;
+            this.styleButton2.Click += new System.EventHandler(this.styleButton2_Click);
+            // 
+            // DigitalClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(856, 450);
+            this.Controls.Add(this.styleButton2);
+            this.Controls.Add(this.styleButton3);
+            this.Controls.Add(this.styleButton1);
+            this.Controls.Add(this.yellowButton);
+            this.Controls.Add(this.greenButton);
+            this.Controls.Add(this.blueButton);
+            this.Controls.Add(this.orangeButton);
+            this.Controls.Add(this.redButton);
+            this.Controls.Add(this.clockLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "DigitalClock";
+            this.Text = "Digital Clock";
+            this.Load += new System.EventHandler(this.DigitalClock_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label clockLabel;
+        private System.Windows.Forms.Timer clockTimer;
+        private System.Windows.Forms.Button redButton;
+        private System.Windows.Forms.Button orangeButton;
+        private System.Windows.Forms.Button blueButton;
+        private System.Windows.Forms.Button greenButton;
+        private System.Windows.Forms.Button yellowButton;
+        private System.Windows.Forms.Button styleButton1;
+        private System.Windows.Forms.Button styleButton3;
+        private System.Windows.Forms.Button styleButton2;
+    }
+}
+
